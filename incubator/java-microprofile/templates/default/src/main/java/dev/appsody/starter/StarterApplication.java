9ae44a3fe3632fa4d5a675c8b0dd3cc5c6630549
Michael C. Thompson <mcthomps@us.ibm.com>
2019-07-02 14:17:56 -0500

    java-microprofile: make the default context root (localhost:9080) respond with something better than a 404
    This change adds a simple Hello World resource to the MicroProfile starter and bind the application to the default context root so that it responds on localhost:9080. When accessed, the user will see "Hello from Appsody!"
    
    Fixes: https://github.com/appsody/stacks/issues/30

--- incubator/java-microprofile/templates/default/src/main/java/dev/appsody/starter/StarterApplication.java
+++ incubator/java-microprofile/templates/default/src/main/java/dev/appsody/starter/StarterApplication.java
@@ -3,7 +3,7 @@ package dev.appsody.starter;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/starter")
+@ApplicationPath("/")
 public class StarterApplication extends Application {
 
 }
