4f7857152cf8bd92a353e650b0e92a8df9f53a60
Sandy <sandykoh99@gmail.com>
2019-08-09 15:40:58 +0100

    stacks: generated index.json for CodeWind

--- ci/build.sh
+++ ci/build.sh
@@ -11,4 +11,5 @@ base_dir="$(cd "$1" && pwd)"
 
 . $base_dir/ci/list.sh $base_dir
 . $base_dir/ci/package.sh $base_dir
+python ./yaml_to_json.py
 . $base_dir/ci/test.sh $base_dir
---
+++ ci/yaml_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import yaml
+import json
+import os
+
+
+with open(os.path.dirname(os.getcwd())+"/index.yaml", 'r') as s, open(os.path.dirname(os.getcwd())+"/index.json", "w") as o:
+    try:
+        doc = yaml.safe_load(s) 
+        o.write(json.dumps(doc, sort_keys = True, indent = 2))
+        print("\n Created index.json file from index.yaml.")
+    except yaml.YAMLError as exc:
+        print(exc)
