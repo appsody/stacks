0a4a026edef33c78ca678b517377c971b619d1e0
Sandy <sandykoh99@gmail.com>
2019-08-12 11:26:58 +0100

    creates index.json file for index.yaml assets

--- ci/yaml_to_json.py
+++ ci/yaml_to_json.py
@@ -3,12 +3,19 @@
 import yaml
 import json
 import os
+import fnmatch
 
 
-with open(os.getcwd()+"/index.yaml", 'r') as s, open(os.getcwd()+"/index.json", "w") as o:
-    try:
-        doc = yaml.safe_load(s) 
-        o.write(json.dumps(doc, sort_keys = True, indent = 2))
-        print("\n Created index.json file from index.yaml.")
-    except yaml.YAMLError as exc:
-        print(exc)
+# directory to store assets for test or release
+assets_dir = os.getcwd()+"/assets"
+
+for file in os.listdir(assets_dir):
+    if fnmatch.fnmatch(file, '*index.yaml'): 
+        with open(assets_dir + "/" + file, 'r') as s, open(assets_dir + "/" + os.path.splitext(file)[0] +".json", "w") as o:
+            try:
+                doc = yaml.safe_load(s) 
+                o.write(json.dumps(doc, sort_keys = True, indent = 2))
+                print("\n Created " + os.path.splitext(file)[0] + ".json file from " + os.path.splitext(file)[0] + ".yaml.")
+            except yaml.YAMLError as exc:
+                print(exc)
+
