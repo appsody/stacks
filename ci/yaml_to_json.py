b054c1366e226972468a68c5436a7282d2861264
Sandy <sandykoh99@gmail.com>
2019-08-13 08:42:17 +0100

    removed comments

--- ci/yaml_to_json.py
+++ ci/yaml_to_json.py
@@ -14,14 +14,10 @@ for file in os.listdir(assets_dir):
     with open(assets_dir + "/" + file, 'r') as s, open(assets_dir + "/" + os.path.splitext(file)[0] + ".json", "w") as o:
         try:
             doc = yaml.safe_load(s)
-            #print doc
             i = 0
+
             if (doc['stacks'] != None):
-                
                 for item in doc['stacks']:
-                    # print item
-                    # print
-                    
                     res = (OrderedDict([
                         ("displayName", item['name']),
                         ("description", item['description']),
@@ -33,12 +29,8 @@ for file in os.listdir(assets_dir):
                                                 ("self", "/devfiles/" + item['id'] + "/devfile.yaml")
                         ]))
                     ]))
+
                     i += 1
-                    # print i
-                    # print len(doc['stacks'])
-                    # print item
-                    # print
-                    # print
                     
                     if i == 1:
                         o.write("[\n")
@@ -48,9 +40,11 @@ for file in os.listdir(assets_dir):
                         o.write("\n")
                         o.write(json.dumps(res, indent = 5))
                         if i == len(doc['stacks']):
-                            o.write("]")
+                            o.write("\n]")
                         else:
                             o.write(",")
+
+                print "Generated " +os.path.splitext(file)[0]+ ".json for " + file
 
         except yaml.YAMLError as exc:
             print(exc)
