f81000aab3cc7239fa69ec87ae57f15881c0ca99
Sandy Koh <sandykoh99@gmail.com>
2019-08-14 12:22:47 +0100

    stacks: generated index.json for CodeWind (#163)
    * stacks: generated index.json for CodeWind
    
    * fixed python command
    
    * fixed python command
    
    * python command
    
    * python command
    
    * creates index.json file for index.yaml assets
    
    * fixed filepath
    
    * fixed filepath
    
    * modified keys and fields
    
    * removed comments
    
    * small typo
    
    * fixed default template
    
    * filepath
    
    * fixed filepath
    
    * ci: fixed code formatting using vscode
    
    Signed-off-by-Neeraj Laad <neeraj.laad@gmail.com>
    
    * ci: renamed files and vars
    
    Signed-off-by: Neeraj Laad <neeraj.laad@gmail.com>
    
    * Update create_codewind_index.py
    
    Signed-off-by Neeraj Laads <neeraj.laad@gmail.com>
    
    * added template name
    
    * added template nam
    
    * clean up

--- ci/build.sh
+++ ci/build.sh
@@ -16,3 +16,5 @@ if [ -z $STACKS_LIST ]; then
 fi
 . $base_dir/ci/package.sh $base_dir
 . $base_dir/ci/test.sh $base_dir
+
+python $base_dir/ci/create_codewind_index.py $base_dir
---
+++ ci/create_codewind_index.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import yaml
+import json
+import os
+import fnmatch
+import sys
+from collections import OrderedDict
+
+base_dir = os.path.abspath(os.path.dirname(sys.argv[0]))
+
+# directory to store assets for test or release
+assets_dir = base_dir + "/assets/"
+
+for file in os.listdir(assets_dir):
+    if fnmatch.fnmatch(file, '*index.yaml'):
+        with open(assets_dir + file, 'r') as yamlFile, open(assets_dir + os.path.splitext(file)[0] + ".json", 'w') as jsonFile:
+            try:
+                doc = yaml.safe_load(yamlFile)
+                list = []
+
+                if (doc['stacks'] != None):
+                    for item in doc['stacks']:
+
+                        # get template name 
+                        for n in range(0, len(item['templates'])):
+                            if len(item['templates'])==1:
+                                template = ""
+                            else:
+                                template = " " + item['templates'][n]['id']
+
+                            # populate stack details
+                            res = (OrderedDict([
+                                ("displayName", "Appsody " + item['name'] + template + " template"),
+                                ("description", item['description']),
+                                ("language", ""),
+                                ("projectType", "appsodyExtension"),
+                                ("projectStyle", "Appsody"),
+                                ("location", item['templates'][n]['url']),
+                                ("links", OrderedDict([
+                                    ("self", "/devfiles/" +
+                                        item['id'] + "/devfile.yaml")
+                                ]))
+                            ]))
+                            list.append(res)
+
+                jsonFile.write(json.dumps(list, indent=4, ensure_ascii=False).encode('utf8'))
+                print("Generated: " + os.path.splitext(file)[0] + ".json")
+
+            except yaml.YAMLError as exc:
+                print(exc)
