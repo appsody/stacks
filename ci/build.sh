f25b88180b53d2b4d9727e2659db7fd14d1eae8a
Sandy <sandykoh99@gmail.com>
2019-08-16 10:09:24 +0100

    changed env var name

--- ci/build.sh
+++ ci/build.sh
@@ -17,6 +17,6 @@ fi
 . $base_dir/ci/package.sh $base_dir
 . $base_dir/ci/test.sh $base_dir
 
-if [ "$CODEWIND_JSON" == "true" ]; then
+if [ "$CODEWIND_INDEX" == "true" ]; then
     python $base_dir/ci/create_codewind_index.py $base_dir
 fi
