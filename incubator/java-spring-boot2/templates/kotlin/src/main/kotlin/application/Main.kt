699944f58edf3e21d47c4fcfffebdbf45e54a699
Erin Schnabel <schnabel@us.ibm.com>
2019-08-19 11:55:17 -0400

    clean up spring boot 2 stack
    Clean up Kotlin support
    Fix liveness endpoints in both templates

---
+++ incubator/java-spring-boot2/templates/kotlin/src/main/kotlin/application/Main.kt
@@ -0,0 +1,12 @@
+package application
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Main
+
+fun main(args: Array<String>) {
+    runApplication<Main>(*args)
+}
+
