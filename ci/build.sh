a3aaee391ff01a14773080a380bbea899a92e0e5
Bazif Khan <51790085+Bazif-Khan@users.noreply.github.com>
2019-08-20 16:27:11 +0100

    build: pass stack as argument for build.sh (#206)
    * build: pass stack as argument for build.sh
    
    * extra space removed
    
    * review changes
    
    * stacklist default issue fixed
    
    * indentation fixed

--- ci/build.sh
+++ ci/build.sh
@@ -3,21 +3,30 @@ set -e
 
 if [ -z "$1" ]
 then
-    echo "One argument is required and must be the base directory of the repository."
-    exit 1
+  echo "One argument is required and must be the base directory of the repository."
+  exit 1
 fi
 
 base_dir="$(cd "$1" && pwd)"
 
+if [ ! -z "$2" ]
+then
+  export STACKS_LIST="$2"
+fi
+
+
 . $base_dir/ci/env.sh
 
-if [ -z $STACKS_LIST ]; then
-    . $base_dir/ci/list.sh $base_dir
+if [ -z "$STACKS_LIST" ]
+then
+  . $base_dir/ci/list.sh $base_dir
+else
+  echo "STACKS_LIST=$STACKS_LIST"
 fi
 . $base_dir/ci/lint.sh $base_dir
 . $base_dir/ci/package.sh $base_dir
 . $base_dir/ci/test.sh $base_dir
 
 if [ "$CODEWIND_INDEX" == "true" ]; then
-    python3 $base_dir/ci/create_codewind_index.py $base_dir
+  python3 $base_dir/ci/create_codewind_index.py $base_dir
 fi
--- ci/lint.sh
+++ ci/lint.sh
@@ -15,85 +15,88 @@ totalWarn=0
 for stack_name in $STACKS_LIST
 do
     stack_dir="$1/$stack_name"
-    image_dir="$stack_dir/image"
-    project_dir="$image_dir/project"
-    template_dir="$stack_dir/templates/*"
+    echo -e "\nLINTING: $stack_name"
 
-    stackName=$(basename -- "$stack_dir")
-    stackName="${stackName%.*}"
-
-    echo -e "\nLINTING $stackName"
-
-    if [ ! -f $stack_dir/stack.yaml ]
+    if [ ! -d $stack_dir ]
     then
-        echo "ERROR: Missing stack.yaml file in $stack_dir"
+        echo "ERROR: Missing stack directory: $1/$stack_name"
         let "error=error+1"
-    fi
+    else
+        image_dir="$stack_dir/image"
+        project_dir="$image_dir/project"
+        template_dir="$stack_dir/templates/*"
 
-    if [ ! -f $stack_dir/README.md ]
-    then
-        echo "ERROR: Missing README.md file in $stack_dir"
-        let "error=error+1"
-    fi
-
-    if [ ! -d $image_dir ]
-    then
-        echo "ERROR: Missing image directory in $stack_dir"
-        let "error=error+1"
-    fi
-
-    if [ ! -f $image_dir/Dockerfile-stack ]
-    then
-        echo "ERROR: Missing Dockerfile-stack in $image_dir"
-        let "error=error+1"
-    fi
-
-    if [ ! -d $project_dir ]
-    then
-        echo "ERROR: Missing project directory in $image_dir"
-        let "error=error+1"
-    fi
-
-    if [ ! -f $project_dir/Dockerfile ]
-    then
-        echo "WARNING: Missing Dockerfile in $project_dir"
-        let "warning=warning+1"
-    fi
-
-    if [ ! -d "$stack_dir/templates" ]
-    then
-        echo "ERROR: Missing templates directory in $stack_dir"
-        let "error=error+1"
-    fi
-
-    for template_list in $template_dir
-    do
-        if [ ! -f $template_list/.appsody-config.yaml ]
+        if [ ! -f $stack_dir/stack.yaml ]
         then
-            templateName=$(basename -- "$template_list")
-            templateName="${templateName%.*}"
-            echo "ERROR: Missing appsody config file in template: $project_dir/templates/$templateName"
+            echo "ERROR: Missing stack.yaml file in $stack_dir"
             let "error=error+1"
         fi
-    done
 
-    if (($error > 0))
-    then
-        let "totalError=error+totalError"
-        echo "LINT FAILED"
-        echo "ERRORS: $error"
-    else
-        echo "LINT PASSED"
-    fi
+        if [ ! -f $stack_dir/README.md ]
+        then
+            echo "ERROR: Missing README.md file in $stack_dir"
+            let "error=error+1"
+        fi
 
-    if (($warning > 0))
-    then
-        let "totalWarn=warning+totalWarn"
-        echo "WARNINGS: $warning"
-    fi
+        if [ ! -d $image_dir ]
+        then
+            echo "ERROR: Missing image directory in $stack_dir"
+            let "error=error+1"
+        fi
 
-    warning=0
-    error=0
+        if [ ! -f $image_dir/Dockerfile-stack ]
+        then
+            echo "ERROR: Missing Dockerfile-stack in $image_dir"
+            let "error=error+1"
+        fi
+
+        if [ ! -d $project_dir ]
+        then
+            echo "ERROR: Missing project directory in $image_dir"
+            let "error=error+1"
+        fi
+
+        if [ ! -f $project_dir/Dockerfile ]
+        then
+            echo "WARNING: Missing Dockerfile in $project_dir"
+            let "warning=warning+1"
+        fi
+
+        if [ ! -d "$stack_dir/templates" ]
+        then
+            echo "ERROR: Missing templates directory in $stack_dir"
+            let "error=error+1"
+        fi
+
+        for template_list in $template_dir
+        do
+            if [ ! -f $template_list/.appsody-config.yaml ]
+            then
+                templateName=$(basename -- "$template_list")
+                templateName="${templateName%.*}"
+                echo "ERROR: Missing appsody config file in template: $project_dir/templates/$templateName"
+                let "error=error+1"
+            fi
+        done
+      fi
+
+      if (($error > 0))
+      then
+          let "totalError=error+totalError"
+          echo "LINT FAILED"
+          echo "ERRORS: $error"
+      else
+          echo "LINT PASSED"
+      fi
+
+      if (($warning > 0))
+      then
+          let "totalWarn=warning+totalWarn"
+          echo "WARNINGS: $warning"
+      fi
+
+      warning=0
+      error=0
 done
 
 echo -e "\nTOTAL ERRORS: $totalError"
