dd70f980a543959c594f1caa6dd532ef6b1ec09a
Bazif Khan <bazif.khan@ibm.com>
2019-08-20 15:47:55 +0100

    indentation fixed

--- ci/lint.sh
+++ ci/lint.sh
@@ -22,81 +22,81 @@ do
         echo "ERROR: Missing stack directory: $1/$stack_name"
         let "error=error+1"
     else
-      image_dir="$stack_dir/image"
-      project_dir="$image_dir/project"
-      template_dir="$stack_dir/templates/*"
+        image_dir="$stack_dir/image"
+        project_dir="$image_dir/project"
+        template_dir="$stack_dir/templates/*"
 
-      if [ ! -f $stack_dir/stack.yaml ]
-      then
-          echo "ERROR: Missing stack.yaml file in $stack_dir"
-          let "error=error+1"
+        if [ ! -f $stack_dir/stack.yaml ]
+        then
+            echo "ERROR: Missing stack.yaml file in $stack_dir"
+            let "error=error+1"
+        fi
+
+        if [ ! -f $stack_dir/README.md ]
+        then
+            echo "ERROR: Missing README.md file in $stack_dir"
+            let "error=error+1"
+        fi
+
+        if [ ! -d $image_dir ]
+        then
+            echo "ERROR: Missing image directory in $stack_dir"
+            let "error=error+1"
+        fi
+
+        if [ ! -f $image_dir/Dockerfile-stack ]
+        then
+            echo "ERROR: Missing Dockerfile-stack in $image_dir"
+            let "error=error+1"
+        fi
+
+        if [ ! -d $project_dir ]
+        then
+            echo "ERROR: Missing project directory in $image_dir"
+            let "error=error+1"
+        fi
+
+        if [ ! -f $project_dir/Dockerfile ]
+        then
+            echo "WARNING: Missing Dockerfile in $project_dir"
+            let "warning=warning+1"
+        fi
+
+        if [ ! -d "$stack_dir/templates" ]
+        then
+            echo "ERROR: Missing templates directory in $stack_dir"
+            let "error=error+1"
+        fi
+
+        for template_list in $template_dir
+        do
+            if [ ! -f $template_list/.appsody-config.yaml ]
+            then
+                templateName=$(basename -- "$template_list")
+                templateName="${templateName%.*}"
+                echo "ERROR: Missing appsody config file in template: $project_dir/templates/$templateName"
+                let "error=error+1"
+            fi
+        done
       fi
 
-      if [ ! -f $stack_dir/README.md ]
+      if (($error > 0))
       then
-          echo "ERROR: Missing README.md file in $stack_dir"
-          let "error=error+1"
+          let "totalError=error+totalError"
+          echo "LINT FAILED"
+          echo "ERRORS: $error"
+      else
+          echo "LINT PASSED"
       fi
 
-      if [ ! -d $image_dir ]
+      if (($warning > 0))
       then
-          echo "ERROR: Missing image directory in $stack_dir"
-          let "error=error+1"
+          let "totalWarn=warning+totalWarn"
+          echo "WARNINGS: $warning"
       fi
 
-      if [ ! -f $image_dir/Dockerfile-stack ]
-      then
-          echo "ERROR: Missing Dockerfile-stack in $image_dir"
-          let "error=error+1"
-      fi
-
-      if [ ! -d $project_dir ]
-      then
-          echo "ERROR: Missing project directory in $image_dir"
-          let "error=error+1"
-      fi
-
-      if [ ! -f $project_dir/Dockerfile ]
-      then
-          echo "WARNING: Missing Dockerfile in $project_dir"
-          let "warning=warning+1"
-      fi
-
-      if [ ! -d "$stack_dir/templates" ]
-      then
-          echo "ERROR: Missing templates directory in $stack_dir"
-          let "error=error+1"
-      fi
-
-      for template_list in $template_dir
-      do
-          if [ ! -f $template_list/.appsody-config.yaml ]
-          then
-              templateName=$(basename -- "$template_list")
-              templateName="${templateName%.*}"
-              echo "ERROR: Missing appsody config file in template: $project_dir/templates/$templateName"
-              let "error=error+1"
-          fi
-      done
-    fi
-
-    if (($error > 0))
-    then
-        let "totalError=error+totalError"
-        echo "LINT FAILED"
-        echo "ERRORS: $error"
-    else
-        echo "LINT PASSED"
-    fi
-
-    if (($warning > 0))
-    then
-        let "totalWarn=warning+totalWarn"
-        echo "WARNINGS: $warning"
-    fi
-
-    warning=0
-    error=0
+      warning=0
+      error=0
 done
 
 echo -e "\nTOTAL ERRORS: $totalError"
