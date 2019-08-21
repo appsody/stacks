8331627e43d9d15f0ef35142dbe5dd8db4b35ad8
Erin Schnabel <schnabel@us.ibm.com>
2019-08-21 07:08:48 -0400

    Nginx image for index + tar.gz
    Assets: flat structure, use version in name
    Non-release artifacts in ci/build directory
    
    Current github releases use a URL like this:
    {{EXTERNAL_URL}}/swift-v0.1.4/incubator.swift.templates.simple.tar.gz
    
    Kabanero uses versions in their archives, so it looks more like this:
    {{EXTERNAL_URL}}/incubator.java-spring-boot2.v0.3.6.templates.default.tar.gz
    
    ./ci/package.sh has been updated to deal with existing template
    archives with and without the version in the filename.
    Archives that are built (either explicitly, or because they are
    missing), will be created with the version in the filename.
    
    ./ci/prefetch.sh will download all of the artifacts referenced by
    index yaml files listed (space separated) in PREFETCH_INDEX_YAML.
    
    Signed-off-by: Erin Schnabel <schnabel@us.ibm.com>

--- ci/env.sh
+++ ci/env.sh
@@ -10,10 +10,38 @@ fi
 
 base_dir="$(cd "$1" && pwd)"
 
-# You can set the following environment variables to specify your docker credentials or docker registry: DOCKER_PASSWORD, DOCKER_USERNAME and DOCKER_REGISTRY
+# ENVIRONMENT VARIABLES (shown with defaults)
 
-# If you would like to generate the Codewind index, set the environment variable CODEWIND_INDEX to 'true'
-# You will need to download the PyYaml module to generate this file
+# Docker credentials for publishing images:
+# export DOCKER_USERNAME
+# export DOCKER_PASSWORD
+# export DOCKER_REGISTRY
+
+# Organization for Docker images
+# export DOCKERHUB_ORG=appsody
+
+# List of apposdy repositories to build indexes for
+# export REPO_LIST="experimental incubator stable"
+
+# git information (determined from current branch if unspecified)
+# export GIT_BRANCH
+# export GIT_ORG_REPO=appsody/stacks
+
+# External/remote URL for downloading git released assets
+# export RELEASE_URL=https://github.com/$GIT_ORG_REPO/releases/download
+
+# Create template archive if it is missing
+# export PACKAGE_WHEN_MISSING=true
+
+# Version or snapshot identifier for appsody-index (ci/package.sh)
+# export INDEX_VERSION=SNAPSHOT
+
+# Base nginx image for appsody-index (ci/nginx/Dockerfile)
+# export NGINX_IMAGE=nginx:stable-alpine
+
+# Build the Codewind index when the value is 'true' (requires PyYaml)
+# export CODEWIND_INDEX
+
 
 #expose an extension point for running before main 'env' processing
 if [ -f $base_dir/ci/ext/pre_env.sh ]
@@ -27,13 +55,65 @@ if [ -z "$REPO_LIST" ]; then
 fi
 
 # dockerhub org for publishing stack
-if [ -z $DOCKERHUB_ORG ]; then
+if [ -z $DOCKERHUB_ORG ]
+then
     export DOCKERHUB_ORG=appsody
 fi
 
-# url for downloading released assets
-if [ -z $RELEASE_URL ]; then
-    export RELEASE_URL="https://github.com/$TRAVIS_REPO_SLUG/releases/download"
+if [ -z $GIT_BRANCH ]
+then
+    if [ -z $TRAVIS_BRANCH ]
+    then
+        export GIT_BRANCH=$(git for-each-ref --format='%(refname:lstrip=2)' "$(git symbolic-ref -q HEAD)")
+    else
+        export GIT_BRANCH=${TRAVIS_BRANCH}
+    fi
+fi
+
+# find github repository slug
+if [ -z $GIT_ORG_REPO ]
+then
+    if [ -z "$TRAVIS_REPO_SLUG" ]
+    then
+        # Find git organization for the current branch
+        git_remote=$(git for-each-ref --format='%(upstream:remotename)' "$(git symbolic-ref -q HEAD)")
+	git_remote=${git_remote:-origin}
+
+        git_remote_url=$(git remote get-url $git_remote)
+        git_remote_url=${git_remote_url:-https://github.com/appsody/stacks.git}
+        git_remote_url=${git_remote_url#*:}
+
+        git_repo=$(basename $git_remote_url .git)
+        git_repo=${git_repo:-stacks}
+
+        git_org=$(basename $(dirname $git_remote_url))
+        git_org=${git_org:-appsody}
+
+        export GIT_ORG_REPO=$git_org/$git_repo
+    else
+        export GIT_ORG_REPO=$TRAVIS_REPO_SLUG
+    fi
+fi
+
+if [ -z "$RELEASE_URL" ]
+then
+    # url for downloading git released assets
+    export RELEASE_URL="https://github.com/$GIT_ORG_REPO/releases/download"
+fi
+
+if [ -z "$PACKAGE_WHEN_MISSING" ] 
+then
+    export PACKAGE_WHEN_MISSING=true
+fi
+
+if [ -z $INDEX_VERSION ]
+then
+    if [ -z $TRAVIS_BUILD_NUMBER ]
+    then
+        export INDEX_VERSION=SNAPSHOT
+    else
+        export INDEX_VERSION=${TRAVIS_BUILD_NUMBER}
+    fi
 fi
 
 #expose an extension point for running after main 'env' processing
---
+++ ci/prefetch.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+#
+# If provided with a URL for an existing index.yaml file, pre-fetch
+# all referenced template archives.
+#
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+assets_dir=${PWD}/assets
+build_dir=${PWD}/build
+
+mkdir -p $assets_dir
+mkdir -p $build_dir
+
+create="1>/dev/null echo"
+verify="1>/dev/null echo"
+if which md5sum > /dev/null
+then
+    create="md5sum -b"
+    verify="md5sum --status -c -"
+elif which shasum > /dev/null
+then
+    create="shasum -b"
+    verify="shasum --status -c -"
+fi
+
+cd $assets_dir
+if [ -n "${PREFETCH_INDEX_YAML}" ]
+then
+    for url in ${PREFETCH_INDEX_YAML}
+    do
+        echo "== $url"
+        index=$(curl -s -L ${url})
+        for x in $(echo "$index" | grep 'url:' )
+        do
+            if [ $x != 'url:' ]
+            then
+                echo "   $x"
+                curl -s -L -O $x
+                filename=$(basename $x)
+                if ! [[ "$filename" =~  v[0-9]+\.[0-9]+\.[0-9]+ ]]
+                then
+                    # the stack version is in the directory name
+                    release=$(basename $(dirname $x))
+                    split=$(echo $release | awk '{split($0,a,"-v"); print a[1],a[2]}')
+                    declare -a stack=($split)
+
+                    # The template id is in the filename: incubator.nodejs-loopback.templates.scaffold.tar.gz
+                    template_id=$(echo $filename | sed -E 's|.*templates\.(.*)\.tar\.gz|\1|')
+
+                    # Create a script that can compare a pre-fetched template version
+                    # to the current stack version (read from stack.yaml), e.g.:
+                    # ./ci/assets/prefetch-stack_id-template_id 0.3.2
+                    echo '#!/bin/bash
+
+cd "'${assets_dir}'"
+# Fetched from '${url}' on '$(date)'
+# '${x}'
+checksum="'$($create $filename)'"
+if echo "$checksum" | '$verify'
+then
+    if [ "$1" == "'${stack[1]}'" ]
+    then
+        echo ok
+    else
+        echo "'${stack[1]}'"
+    fi
+else
+    echo nomatch
+fi
+' > ${build_dir}/prefetch-"${stack[0]}-${template_id}"
+                    chmod +x ${build_dir}/prefetch-"${stack[0]}-${template_id}"
+                fi
+            fi
+        done
+    done
+fi
+
