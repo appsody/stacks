8ce91fbcd8bf1a62bcc5209648b5d003c18a4ea1
Erin Schnabel <schnabel@us.ibm.com>
2019-08-31 01:59:56 -0400

    java-spring-boot2: init script path; hush progress
    Signed-off-by: Erin Schnabel <schnabel@us.ibm.com>

--- incubator/java-spring-boot2/image/project/.appsody-init.bat
+++ incubator/java-spring-boot2/image/project/.appsody-init.bat
@@ -1,4 +1,4 @@
 if not exist %SystemDrive%%HOMEPATH%\.m2\repository\ (
   mkdir %SystemDrive%%HOMEPATH%\.m2\repository
 )
-mvnw install -q -f /project/appsody-boot2-pom.xml
+mvnw install -q -f ./appsody-boot2-pom.xml
