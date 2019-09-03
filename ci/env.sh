84cdf413e8985aac25e10a815879202fcfb8c971
Musa Hamwala <musahamwala@icloud.com>
2019-09-03 11:12:01 +0100

    fix typo

--- ci/env.sh
+++ ci/env.sh
@@ -88,7 +88,7 @@ fi
 # image registry org for publishing stack
 if [ -z "$IMAGE_REGISTRY_ORG" ]
 then
-    export IMAGE_REGISTRY_ORG=mhamwala
+    export IMAGE_REGISTRY_ORG=appsody
 fi
 
 if [ -z $GIT_BRANCH ]
