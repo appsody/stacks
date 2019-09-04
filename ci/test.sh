12974448755783b897d5ffd5bc4f0ded2fc136fc
Erin Schnabel <schnabel@us.ibm.com>
2019-09-03 21:32:05 -0400

    Run appsody commands (build, run, stop) for specified stacks
    Provide basic contents in test.sh to ensure that stack templates
    can be run, and that projects can be extracted and built using
    the stack.
    
    Travis builds:
    - Install appsody CLI if missing.
    - Use a subset of known stacks when nothing else is specified
    - Wrap long-running commands to prevent exit from no console output
    
    Signed-off-by: Erin Schnabel <schnabel@us.ibm.com>

--- ci/test.sh
+++ ci/test.sh
@@ -6,8 +6,135 @@
 # expose an extension point for running before main 'test' processing
 exec_hooks $script_dir/ext/pre_test.d
 
-echo -e "\nNo tests implemented yet"
+pull_policy=$APPSODY_PULL_POLICY
+
+if ! which appsody
+then
+    echo "appsody CLI not found on the path"
+    return 1
+fi
+
+wait_until_ready() {
+    i=0
+    while true
+    do
+        if eval $1
+        then
+            break
+        else
+            ((i=i+1))
+            if [ $i -gt 60 ]
+            then
+                printf '\n'
+                return 1
+            fi
+            printf '.'
+            sleep 2s
+        fi
+    done
+    printf '\n'
+    return 0
+}
+
+test_template() {
+    local template=$2
+    local stack=$(basename $1)
+    local index=$(dirname $1)-index
+    local index_local=$(dirname $1)-index-local
+    local error=0
+
+    echo ""
+    echo "=== Testing $stack : $template"
+    echo ""
+
+    if appsody list $index_local | grep -q $stack
+    then
+        local repo=$index_local
+        export APPSODY_PULL_POLICY=$pull_policy
+    else
+        local repo=$index
+        export APPSODY_PULL_POLICY=ALWAYS
+    fi
+
+    rm -rf $build_dir/test/$repo/$stack/$template
+    mkdir -pv $build_dir/test/$repo/$stack/$template
+    pushd $build_dir/test/$repo/$stack/$template
+
+    echo ""
+    echo "> appsody init -v $repo/$stack $template"
+    echo ""
+    if ${CI_WAIT_FOR} appsody init -v --overwrite $repo/$stack $template
+    then
+        echo ""
+        echo "> appsody run -P --name $stack-$template"
+        echo ""
+        2>run.log 1>&2 appsody run -P --name $stack-$template &
+
+        echo "Waiting for container to start"
+        if ! wait_until_ready "appsody ps | grep -q $stack-$template"
+        then
+            cat run.log
+            ((error=error+1))
+        fi
+        echo error=$error
+        appsody ps
+
+        echo ""
+        echo "> appsody stop --name $stack-$template"
+        echo ""
+        appsody stop --name $stack-$template
+        if ! wait_until_ready "appsody ps | grep -q $stack-$template; [ \$? -eq 1 ]"
+        then
+            ((error=error+1))
+        fi
+        echo error=$error
+        appsody ps
+
+        echo ""
+        echo "> appsody build"
+        echo ""
+        if ! ${CI_WAIT_FOR} appsody build
+        then
+            ((error=error+1))
+        fi
+        echo error=$error
+
+        echo "Finished with ${error} error(s)"
+    else
+        echo "Unable to initialize project"
+        exit 1
+    fi
+    popd
+}
+
+for x in $assets_dir/*.yaml
+do
+    if [ -f $x ] && grep template $x > /dev/null
+    then
+        index_url=file://$x
+        y=$(basename $x)
+        if appsody repo list | grep -q -e "^\*\?${y%.*}"
+        then
+            echo "Repo ${y%.*} exists"
+        else
+            echo "Installing repo ${y%.*} : $index_url"
+            appsody repo add ${y%.*} $index_url
+        fi
+    fi
+done
+
+appsody repo list
+export APPSODY_PULL_POLICY=IFNOTPRESENT
+
+for stack in $STACKS_LIST
+do
+    for template in "$stack/templates/*"
+    do
+        test_template "$stack" "$(basename $template)"
+    done
+done
+
+export APPSODY_PULL_POLICY=$pull_policy
 
 # expose an extension point for running after main 'test' processing
 exec_hooks $script_dir/ext/post_test.d
-
