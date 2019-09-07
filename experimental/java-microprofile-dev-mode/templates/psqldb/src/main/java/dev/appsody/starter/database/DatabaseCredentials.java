77a78101440e9921783e51b44dd026a856bdb109
Denilson Nastacio <dnastaci@us.ibm.com>
2019-09-06 22:57:38 -0400

    Add psqldb template

---
+++ experimental/java-microprofile-dev-mode/templates/psqldb/src/main/java/dev/appsody/starter/database/DatabaseCredentials.java
@@ -0,0 +1,17 @@
+package dev.appsody.starter.database;
+
+public class DatabaseCredentials {
+
+    public String db;
+    public String dsn;
+    public String host;
+    public String hostname;
+    public String https_url;
+    public String jdbcurl;
+    public String password;
+    public int    port;
+    public String ssldsn;
+    public String ssljdbcurl;
+    public String uri;
+    public String username;
+}
