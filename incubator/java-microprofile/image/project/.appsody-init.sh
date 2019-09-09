22ddd8113da29d0347cf958bb98081ebafd8d954
Ozzy Osborne <ozzy@ca.ibm.com>
2019-09-09 16:28:57 -0400

    Make mvnw usage optional on java presence

--- incubator/java-microprofile/image/project/.appsody-init.sh
+++ incubator/java-microprofile/image/project/.appsody-init.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-./mvnw install -Denforcer.skip=true
+which java 2>&1 >/dev/null ; JAVA_KNOWN=$?
+if [ ! -z "$JAVA_HOME" ] || [ $JAVA_KNOWN = "0" ]; then
+  ./mvnw install -Denforcer.skip=true
+fi
