d349db7b11e3eb8aae270781d4d44d5cdf671f42
Neeraj Laad <neeraj.laad@gmail.com>
2019-09-11 11:21:16 +0100

    java-microprofile: version range
    Signed-off-ny Neeraj Laad

--- incubator/java-microprofile/image/project/validate.sh
+++ incubator/java-microprofile/image/project/validate.sh
@@ -1,40 +1,48 @@
 #!/bin/bash
 
-# Test pom.xml is present and a file.
-if [ ! -f ./pom.xml ]; then
+parent_pom=../pom.xml
+child_pom=pom.xml
+
+# Test child pom file is present
+if [ ! -f ${child_pom} ]; then
   echo "Error: Could not find Maven pom.xml
 
   * The project directory (containing an .appsody-conf.yaml file) must contain a pom.xml file.
-  * On Windows and MacOS, the project directory should also be shared with Docker: 
+  * On Windows and MacOS, the project directory should also be shared with Docker:
     - Win: https://docs.docker.com/docker-for-windows/#shared-drives
     - Mac: https://docs.docker.com/docker-for-mac/#file-sharing
   "
-  exit 1   
+  exit 1
 fi
 
-# Get parent pom information (../pom.xml)
+# Get parent pom file information
 args='export PARENT_GROUP_ID=${project.groupId}; export PARENT_ARTIFACT_ID=${project.artifactId}; export PARENT_VERSION=${project.version}
 export LIBERTY_GROUP_ID=${liberty.groupId}; export LIBERTY_ARTIFACT_ID=${liberty.artifactId}; export LIBERTY_VERSION=${version.openliberty-runtime}'
-eval $(mvn -q -Dexec.executable=echo -Dexec.args="${args}" --non-recursive -f ../pom.xml exec:exec 2>/dev/null)
+eval $(mvn -q -Dexec.executable=echo -Dexec.args="${args}" --non-recursive -f ${parent_pom} exec:exec 2>/dev/null)
 
 # Install parent pom
 echo "Installing parent ${PARENT_GROUP_ID}:${PARENT_ARTIFACT_ID}:${PARENT_VERSION}"
-mvn install -Dmaven.repo.local=/mvn/repository -Denforcer.skip=true -f ../pom.xml
+mvn install -Dmaven.repo.local=/mvn/repository -Denforcer.skip=true -f ${parent_pom}
+
+major="$(echo ${PARENT_VERSION} | cut -d'.' -f1)"
+minor="$(echo ${PARENT_VERSION} | cut -d'.' -f2)"
+((next=minor+1))
+version_range="[${major}.${minor},${major}.${next})"
 
 # Check child pom for required parent project
-if ! grep -Gz "<parent>.*<groupId>${PARENT_GROUP_ID}</groupId>.*</parent>" pom.xml | grep -Gz "<parent>.*<artifactId>${PARENT_ARTIFACT_ID}</artifactId>.*</parent>" | grep -Gzq "<parent>.*<version>${PARENT_VERSION}</version>.*</parent>"
+if ! grep -Gz "<parent>.*<groupId>${PARENT_GROUP_ID}</groupId>.*</parent>" ${child_pom} | grep -Gz "<parent>.*<artifactId>${PARENT_ARTIFACT_ID}</artifactId>.*</parent>" | grep -Gzq "<parent>.*<version>${version_range}</version>.*</parent>"
 then
   echo "Project is missing required parent:
   <parent>
     <groupId>${PARENT_GROUP_ID}</groupId>
     <artifactId>${PARENT_ARTIFACT_ID}</artifactId>
-    <version>${PARENT_VERSION}</version>
+    <version>${version_range}</version>
   </parent>"
   exit 1
 fi
 
 # Check child pom for required liberty version, groupID and artifactId
-if ! grep -Gz "<plugin>.*<artifactId>liberty-maven-plugin</artifactId>.*<groupId>${LIBERTY_GROUP_ID}</groupId>\|<groupId>\${liberty.groupId}</groupId>.*</plugin>" pom.xml | grep -Gz "<plugin>.*<artifactId>liberty-maven-plugin</artifactId>.*<artifactId>${LIBERTY_ARTIFACT_ID}</artifactId>\|<artifactId>\${liberty.artifactId}</artifactId>.*</plugin>" | grep -Gzq "<plugin>.*<artifactId>liberty-maven-plugin</artifactId>.*<version>${LIBERTY_VERSION}</version>\|<version>\${version.openliberty-runtime}</version>.*</plugin>"
+if ! grep -Gz "<plugin>.*<artifactId>liberty-maven-plugin</artifactId>.*<groupId>${LIBERTY_GROUP_ID}</groupId>\|<groupId>\${liberty.groupId}</groupId>.*</plugin>" ${child_pom} | grep -Gz "<plugin>.*<artifactId>liberty-maven-plugin</artifactId>.*<artifactId>${LIBERTY_ARTIFACT_ID}</artifactId>\|<artifactId>\${liberty.artifactId}</artifactId>.*</plugin>" | grep -Gzq "<plugin>.*<artifactId>liberty-maven-plugin</artifactId>.*<version>${LIBERTY_VERSION}</version>\|<version>\${version.openliberty-runtime}</version>.*</plugin>"
 then
   echo "Project is not using the right OpenLiberty assembly artifact:
   <assemblyArtifact>
@@ -53,7 +61,7 @@ Alternatively you could also use these properties:
 fi
 
 # Enforcing loose application
-if ! grep -Gzq "<configuration.*<looseApplication>true</looseApplication>.*</configuration" pom.xml
+if ! grep -Gzq "<configuration.*<looseApplication>true</looseApplication>.*</configuration" ${child_pom}
 then
   echo "Should be a loose application:
   <configuration>
