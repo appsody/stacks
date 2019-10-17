e10cf0da8f8c8499afe9d61a724d22c3267d6dbe
David Jones <djones6@uk.ibm.com>
2019-10-17 16:52:53 +0100

    Avoid unnecessary references to base image

--- incubator/kitura/image/project/.appsody-init.sh
+++ incubator/kitura/image/project/.appsody-init.sh
@@ -5,15 +5,7 @@
 #
 set -e
 
-# TODO: This should be a reference to the project's stack image
-IMAGE="appsody/kitura"
-
-# Create a temporary container in order to copy files from the stack image
-docker create --name kitura-image-temp $IMAGE >/dev/null
-
 # Current directory is .appsody-init, which is a child of the user's
 # project directory, so we copy the dependencies to a .appsody directory
 # one level up.
-docker cp kitura-image-temp:/project/deps ../.appsody
-
-docker rm kitura-image-temp >/dev/null
+cp -R -p ./deps ../.appsody
