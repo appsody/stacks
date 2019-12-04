d5a723c86791763433a35b4a95a659b3dfeb6789
CameronMcWilliam <cam.mcwilliam@me.com>
2019-12-04 09:39:37 +0000

    Removed swift 4.1 version check

--- incubator/kitura/templates/default/Tests/ApplicationTests/KituraStackTests.swift
+++ incubator/kitura/templates/default/Tests/ApplicationTests/KituraStackTests.swift
@@ -3,11 +3,10 @@ import Kitura
 import KituraNet
 import XCTest
 import AppsodyKitura
-#if swift(>=4.1)
-   #if canImport(FoundationNetworking)
-     import FoundationNetworking
-   #endif
+#if canImport(FoundationNetworking)
+   import FoundationNetworking
 #endif
+
 
 @testable import Application
 
