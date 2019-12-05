6223508712b65b0877289236898a1b3ab9f740a7
Sam Roberts <vieuxtech@gmail.com>
2019-12-05 14:11:37 -0800

    nodejs-express: add pino logger and middleware

--- incubator/nodejs-express/templates/simple/app.js
+++ incubator/nodejs-express/templates/simple/app.js
@@ -1,10 +1,12 @@
 module.exports = (/*options*/) => {
-  // Use options.server to access http.Server. Example of socket.io:
+  // Use options.server to access http.Server. Example with socket.io:
   //     const io = require('socket.io')(options.server)
   const app = require('express')()
 
   app.get('/', (req, res) => {
-    res.send("Hello from Appsody!");
+    // Use req.log (a `pino` instance) to log JSON:
+    req.log.info({message: 'Hello from Appsody!'});
+    res.send('Hello from Appsody!');
   });
 
   return app;
