66313953ee40dc086c95327064a544aed6f668e0
BarDweller <bardweller@gmail.com>
2019-12-09 13:18:41 -0500

    Correct init scripts

--- experimental/java-spring-boot2-liberty/image/project/.appsody-init.bat
+++ experimental/java-spring-boot2-liberty/image/project/.appsody-init.bat
@@ -1,6 +1,14 @@
-setlocal
-set JAVA_FOUND=0
+if not exist %SystemDrive%%HOMEPATH%\.m2\repository\ (
+  mkdir %SystemDrive%%HOMEPATH%\.m2\repository
+)
+
+copy mvnw*.* .. >nul
+mkdir ..\.mvn
+robocopy .mvn ..\.mvn /e /njh /njs /ndl /nc /ns >nul
+
 where java >nul 2>nul
-if %ERRORLEVEL% EQU 0 set JAVA_FOUND=1
-if defined JAVA_HOME set JAVA_FOUND=1
-if "%JAVA_FOUND%"==1 mvnw install -Denforcer.skip=true
+if %ERRORLEVEL% EQU 0 (
+  if defined JAVA_HOME (
+    ..\mvnw install -Denforcer.skip=true
+  )
+)
