2a9892e335160f63d8daede5015203ba3dd9ebb6
Erin Schnabel <schnabel@us.ibm.com>
2019-12-12 14:10:35 -0500

    java-spring-boot2: Use templating, image layers

--- incubator/java-spring-boot2/image/project/java-spring-boot2-build.sh
+++ incubator/java-spring-boot2/image/project/java-spring-boot2-build.sh
@@ -37,9 +37,14 @@ run_mvn () {
   mvn --no-transfer-progress ${MVN_COLOR} "$@"
 }
 
+exec_run_mvn () {
+  echo -e "${GREEN}> mvn $@${NO_COLOR}"
+  exec mvn --no-transfer-progress ${MVN_COLOR} "$@"
+}
+
 common() {
   # Test pom.xml is present and a file.
-  if [ ! -f ./pom.xml ]; then
+  if [ ! -f ./pom.xml ] && [ ! -z "${APPSODY_DEV_MODE}" ]; then
     error "Could not find Maven pom.xml
 
     * The project directory (containing an .appsody-conf.yaml file) must contain a pom.xml file.
@@ -50,7 +55,7 @@ common() {
     exit 1
   fi
   # workaround: exit with error if repository does not exist
-  if [ ! -d /mvn/repository ]; then
+  if [ ! -d /mvn/repository ] && [ ! -z "${APPSODY_DEV_MODE}" ]; then
     error "Could not find local Maven repository
 
     Create a .m2/repository directory in your home directory. For example:
@@ -72,13 +77,8 @@ common() {
   if ! $(mvn -N dependency:get -q -o -Dartifact=${a_groupId}:${a_artifactId}:${a_version} -Dpackaging=pom >/dev/null)
   then
     # Install parent pom
-    note "Installing parent ${a_groupId}:${a_artifactId}:${a_version}"
-    if [ -z "${APPSODY_DEV_MODE}" ]
-    then
-        run_mvn install -f /project/appsody-boot2-pom.xml
-    else
-        run_mvn install -q -f /project/appsody-boot2-pom.xml
-    fi
+    note "Installing parent ${a_groupId}:${a_artifactId}:${a_version} and required dependencies..."
+    run_mvn install -q -f /project/appsody-boot2-pom.xml
   fi
 
   local p_groupId=$(xmlstarlet sel -T -N x="http://maven.apache.org/POM/4.0.0" -t -v "/x:project/x:parent/x:groupId" pom.xml)
@@ -119,30 +119,47 @@ the parent version in pom.xml, and test your changes.
 
 recompile() {
   note "Compile project in the foreground"
-  run_mvn compile
+  exec_run_mvn compile
 }
 
 package() {
-  note "Package project in the foreground"
-  run_mvn clean package verify
+  local group_id=$(xmlstarlet sel -T -N x="http://maven.apache.org/POM/4.0.0" -t -v "/x:project/x:groupId" pom.xml)
+  local artifact_id=$(xmlstarlet sel -T -N x="http://maven.apache.org/POM/4.0.0" -t -v "/x:project/x:artifactId" pom.xml)
+  local artifact_version=$(xmlstarlet sel -T -N x="http://maven.apache.org/POM/4.0.0" -t -v "/x:project/x:version" pom.xml)
+  note "Packaging and verifying application ${group_id}:${artifact_id}:${artifact_version}"
+  run_mvn clean package
+  mkdir -p target/dependency && (cd target/dependency; jar -xf ../*.jar)
+}
+
+createStartScript() {
+  if [ ! -d target/dependency ]; then
+    error "Application must be packaged first"
+    exit 1
+  fi
+  echo '#!/bin/sh' > target/start.sh
+  echo 'exec java $JVM_ARGS -cp /app:/app/lib/* -Djava.security.egd=file:/dev/./urandom \' >> target/start.sh
+  cat target/dependency/META-INF/MANIFEST.MF | grep 'Start-Class: ' | cut -d' ' -f2 | tr -d '\r\n' >> target/start.sh
+  echo "" >> target/start.sh
+  cat target/start.sh
+  chmod +x target/start.sh
 }
 
 debug() {
   note "Build and debug project in the foreground"
-  run_mvn -Dmaven.test.skip=true \
+  exec_run_mvn -Dmaven.test.skip=true \
     -Dspring-boot.run.jvmArguments='-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005' \
     spring-boot:run
 }
 
 run() {
   note "Build and run project in the foreground"
-  run_mvn -Dmaven.test.skip=true \
+  exec_run_mvn -Dmaven.test.skip=true \
     clean spring-boot:run
 }
 
 test() {
   note "Test project in the foreground"
-  run_mvn package test
+  exec_run_mvn package test
 }
 
 #set the action, default to fail text if none passed.
@@ -160,6 +177,9 @@ case "${ACTION}" in
   package)
     common
     package
+  ;;
+  createStartScript)
+    createStartScript
   ;;
   debug)
     common
