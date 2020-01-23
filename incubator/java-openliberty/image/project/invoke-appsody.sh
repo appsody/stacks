7e2a06bb89dab9d413313c612b009c523df23f26
Andrew J. Mauer <ajmauer@gmail.com>
2020-01-23 17:10:22 -0500

    update message for appsody run, debug and test invocations

--- incubator/java-openliberty/image/project/invoke-appsody.sh
+++ incubator/java-openliberty/image/project/invoke-appsody.sh
@@ -10,23 +10,23 @@ case $APPSODY_DEV_MODE in
 		#if [ -e /project/user-app/.appsody-nolocal ]
 		if [ $BIN_TEMPLATE = 1 ]
 		then
-			exec echo this appsody run not supported when using binary template
+			echo this appsody run not supported when using binary template. Press ctl-C and/or stop the current container.
 		else
 			mvn -B -Pstack-image-run -DappsDirectory=apps -Dmaven.repo.local=/mvn/repository liberty:dev
 		fi
 		;;
 	debug)
-		if [ -e /project/user-app/.appsody-nolocal ]
+		if [ $BIN_TEMPLATE = 1 ]
 		then
-			echo appsody debug not supported when using binary template
+			echo this appsody run not supported when using binary template. Press ctl-C and/or stop the current container.
 		else
 			mvn -B -Pstack-image-run -DappsDirectory=apps -Dmaven.repo.local=/mvn/repository liberty:dev
 		fi
 		;;
 	test)
-		if [ -e /project/user-app/.appsody-nolocal ]
+		if [ $BIN_TEMPLATE = 1 ]
 		then
-			echo appsody test not supported when using binary template
+			echo this appsody run not supported when using binary template. Press ctl-C and/or stop the current container.
 		else
 			exec mvn -B -Pstack-image-run -DappsDirectory=apps -Dmaven.repo.local=/mvn/repository clean package liberty:create liberty:install-feature liberty:start liberty:deploy failsafe:integration-test@it-exec liberty:stop failsafe:verify
 		fi
