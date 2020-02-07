20d2a0502fa82d9ced489238322ff9c7c58fd9b9
kilnerm <38245247+kilnerm@users.noreply.github.com>
2020-02-07 15:19:07 +0000

    Yq fix (#261)
    * add script for yq install
    
    * install yq via script
    
    * try sudo
    
    * use correct yq level
    
    * fix loopback
    
    * remove node loopback test

---
+++ ci/install_yq.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd /tmp
+wget https://github.com/mikefarah/yq/releases/download/2.4.1/yq_linux_amd64
+sudo cp ./yq_linux_amd64 /usr/local/bin/yq
+sudo chmod 755 /usr/local/bin/yq
+
+yq --version
