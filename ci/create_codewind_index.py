43f848131cfc60b92e426d1e017cc20fd33ede35
Sandy <sandykoh99@gmail.com>
2020-02-14 09:50:22 +0000

    requested changes

--- ci/create_codewind_index.py
+++ ci/create_codewind_index.py
@@ -11,16 +11,19 @@ from argparse import ArgumentDefaultsHelpFormatter
 parser = argparse.ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
 
 parser.add_argument("-n", "--namePrefix", help="Display name prefix.", default="Appsody")
-parser.add_argument("-f", "--file", help="Location of yaml files.", default=os.getcwd())
+parser.add_argument("-f", "--file", help="Absolute or relative path, to a yaml file or directory of yaml files.", default=os.getcwd())
 
 args = parser.parse_args()
 
 displayNamePrefix = args.namePrefix
 
-yaml_dir = args.file + "/"
+yaml_dir = args.file
+#remove trailing "/" from file arg if exists
+if args.file[-1] == "/":
+    yaml_dir = args.file[:-1]
 
 def generate_json():
-    with open(yaml_dir + file, 'r') as yamlFile, open(yaml_dir + os.path.splitext(file)[0] + ".json", 'wb') as jsonFile:
+    with open(inputFile, 'r') as yamlFile, open(inputFile.rsplit('.', 1)[0] + ".json", 'wb') as jsonFile:
             try:
                 doc = yaml.safe_load(yamlFile)
                 list = []
@@ -51,16 +54,15 @@ def generate_json():
                             list.append(res)
 
                 jsonFile.write(json.dumps(list, indent=4, ensure_ascii=False).encode('utf8'))
-                print("Generated: " + os.path.splitext(file)[0] + ".json")
-
+                print("Generated: " + inputFile.rsplit('.', 1)[0] + ".json")
             except yaml.YAMLError as exc:
                 print(exc)
 
 if os.path.isdir(yaml_dir):
     for file in os.listdir(yaml_dir):
         if fnmatch.fnmatch(file, '*.yaml'):
+            inputFile = yaml_dir + "/" + file
             generate_json()
 else:
-    yaml_dir = yaml_dir.rsplit('/', 2)[0] + "/"
-    file = args.file.split('/')[-1]
+    inputFile = yaml_dir
     generate_json()
