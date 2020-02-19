b334e19dc55728ec0e3ae0f118a71811676d2ceb
Steven Groeger <groeges@uk.ibm.com>
2020-02-19 12:13:43 +0000

    Remove nodejs-loopback stack from repository
    Signed-off-by: Steven Groeger <groeges@uk.ibm.com>

--- incubator/nodejs-loopback/templates/scaffold/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: '@loopback/eslint-config',
-  parserOptions: {
-    // See https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/parser#configuration
-    createDefaultProgram: true,
-  },
-};
