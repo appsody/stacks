d0adc60c60dc79e2711310d3c8796277b790c432
Terrence Nixa <tnixa@us.ibm.com>
2020-03-24 16:35:13 -0500

    initial blockchain stack

---
+++ incubator/blockchain-client-openliberty/templates/default/src/main/java/dev/appsody/starter/StarterApplication.java
@@ -0,0 +1,9 @@
+package dev.appsody.starter;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/blockchain")
+public class StarterApplication extends Application {
+    
+}
---
+++ incubator/blockchain-client-openliberty/templates/default/src/main/java/dev/appsody/starter/exceptions/AssetExceptionMapper.java
@@ -0,0 +1,26 @@
+package dev.appsody.starter.exceptions;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class AssetExceptionMapper implements ExceptionMapper<Throwable> {
+   
+  @Override
+  public Response toResponse(Throwable exception) {
+      ExceptionResponse response = null;
+      if (exception instanceof AssetNotFoundException){
+        response = new ExceptionResponse(exception.getMessage());
+        return Response.status(Status.NOT_FOUND).entity(response).build();
+      }
+      if (exception instanceof AssetException){
+        response = new ExceptionResponse(exception.getMessage());
+        return Response.status(Status.BAD_REQUEST).entity(response).build();
+      }
+
+      return Response.serverError().entity(exception).build();
+      
+    }
+}
