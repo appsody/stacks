fbee5c2dc465aaaa1ab4ba485b531f6d380bfb5d
Michele Chilanti <chilantim@gmail.com>
2020-04-03 10:37:28 -0500

    Asset change

--- incubator/blockchain-client-openliberty/templates/default/src/main/java/dev/appsody/starter/StarterResource.java
+++ incubator/blockchain-client-openliberty/templates/default/src/main/java/dev/appsody/starter/StarterResource.java
@@ -2,12 +2,15 @@ package dev.appsody.starter;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import application.model.Asset;
 
 @Path("/resource")
 public class StarterResource {
 
     @GET
     public String getRequest() {
-        return "StarterResource response";
+        Asset asset = new Asset();
+        asset.setAssetName("TEST");
+        return "StarterResource: "+asset.getAssetName();
     }
 }
