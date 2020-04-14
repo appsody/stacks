d71eeb59e4fa9f94f0e0cd9b88e18271e99ab1b2
Terrence Nixa <tnixa@us.ibm.com>
2020-04-14 16:47:19 -0500

    inmemory wallet exceptions

--- incubator/blockchain-client-openliberty/templates/default/src/main/java/application/wm/InMemoryWallet.java
+++ incubator/blockchain-client-openliberty/templates/default/src/main/java/application/wm/InMemoryWallet.java
@@ -5,6 +5,7 @@ import java.io.StringReader;
 import java.util.Base64;
 
 import org.hyperledger.fabric.gateway.Wallet;
+import org.hyperledger.fabric_ca.sdk.exception.IdentityException;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -14,60 +15,67 @@ import application.cm.ConnectionConfiguration;
 public class InMemoryWallet implements WalletManager {
 
     @Override
-    public Wallet getWallet() {
+    public Wallet getWallet() throws IdentityException {
         Wallet wallet = Wallet.createInMemoryWallet();
         Wallet.Identity identity = null;
-        // TODO - Util function to retrieve/validate these JSONObjects from strings
 
-        JSONObject walletCredential;
-        JSONArray walletCredentials;
         String walletCredentialsString = ConnectionConfiguration.getWalletCredentials();
+        if (walletCredentialsString == null || walletCredentialsString.isEmpty()) {
+            throw new IdentityException("Wallet credentials not found.");
+        }
+        JSONArray walletCredentials = null;
      
+        // parse wallet identities
         try {
             walletCredentials = new JSONArray(walletCredentialsString);
-        } catch (JSONException excpt) { //TODO - should log and throw?
-            return null;
+        } catch (JSONException e) {
+            throw new IdentityException("Error parsing wallet credentials.", e);
         } 
         
         // process each identity
         for (Object walletCredentialObj: walletCredentials) {
-            walletCredential = (JSONObject) walletCredentialObj;
             StringReader certificateRdr = null;
             StringReader privateKeyRdr = null;
+            String mspId = null;
+            String id = null;
 
-            String certificate = walletCredential.getString("cert");
-            if (certificate != null && !certificate.isEmpty()) {
-                byte[] encoded = Base64.getDecoder().decode(certificate);
-                certificateRdr = new StringReader(new String(encoded));
+            try {
+                JSONObject walletCredential = (JSONObject) walletCredentialObj;
+
+                String certificate = walletCredential.getString("cert");
+                if (certificate != null && !certificate.isEmpty()) {
+                    byte[] encoded = Base64.getDecoder().decode(certificate);
+                    certificateRdr = new StringReader(new String(encoded));
+                } else {
+                    throw new IdentityException("Invalid certificate provided.");
+                }
+                
+                String privateKey = walletCredential.getString("private_key");
+                if (privateKey != null && !privateKey.isEmpty()) {
+                    byte[] encoded = Base64.getDecoder().decode(privateKey);
+                    privateKeyRdr = new StringReader(new String(encoded));
+                } else {
+                    throw new IdentityException("Invalid private key provided.");
+                }
+
+                mspId = walletCredential.getString("msp_id");
+                if (mspId == null || mspId.isEmpty()) {
+                    throw new IdentityException("Invalid msp id provided.");
+                }
+
+                id = walletCredential.getString("name");
+                if (id == null || id.isEmpty()) {
+                    throw new IdentityException("Invalid id provided");
+                } 
+            } catch (JSONException | IllegalArgumentException | ClassCastException  e) {
+                throw new IdentityException("Exception processing identity.", e);
             }
-            
-            String privateKey = walletCredential.getString("private_key");
-            if (privateKey != null && !privateKey.isEmpty()) {
-                byte[] encoded = Base64.getDecoder().decode(privateKey);
-                privateKeyRdr = new StringReader(new String(encoded));
-            }
 
-            String mspId = walletCredential.getString("msp_id");
-            if (mspId == null || mspId.isEmpty()) {
-                // todo handle missing case
-            }
-
-            String id = walletCredential.getString("name");
-            if (id == null || id.isEmpty()) {
-                // todo handle missing case
-            }     
-
-    //TODO - proper exception handling
             try {
                 identity = Wallet.Identity.createIdentity(mspId, certificateRdr, privateKeyRdr);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            
-            try {
                 wallet.put(id, identity);
             } catch (IOException e) {
-                e.printStackTrace();
+                throw new IdentityException("Exception creating wallet identity.", e);
             }
         }
         return wallet;
