5ad218f331fb68748badb186dd16a7c235fc2b1b
Terrence Nixa <tnixa@us.ibm.com>
2020-04-15 10:54:12 -0500

    connection manager exception handling

--- incubator/blockchain-client-openliberty/templates/default/src/main/java/application/cm/ConnectionManager.java
+++ incubator/blockchain-client-openliberty/templates/default/src/main/java/application/cm/ConnectionManager.java
@@ -6,48 +6,56 @@ import java.util.HashMap;
 
 import org.hyperledger.fabric.gateway.Contract;
 import org.hyperledger.fabric.gateway.Gateway;
+import org.hyperledger.fabric.gateway.GatewayException;
+import org.hyperledger.fabric.gateway.GatewayRuntimeException;
+import org.hyperledger.fabric.gateway.Network;
 import org.hyperledger.fabric.gateway.Wallet;
+import org.hyperledger.fabric_ca.sdk.exception.IdentityException;
 
 import application.wm.WalletManagerDelegate;
 
 public class ConnectionManager {
 
-    private static HashMap<String, Gateway> gateways;
+    private static HashMap<String, Gateway> gateways = new HashMap<String, Gateway>();
 
-    static {
-        gateways = new HashMap<String, Gateway>();
-    }
-
-    public static Gateway getGateway(String id) {
-        Gateway gateway;
+    public static Gateway getGateway(String fabricId) throws IdentityException, GatewayException {
+        Gateway gateway = null;
 
         synchronized (gateways) {
-            gateway = gateways.get(id);
-            Gateway.Builder builder;
-            if (gateway == null) { // Call wallet manager here
-                ByteArrayInputStream connProfileIS = new ByteArrayInputStream(
-                        ConnectionConfiguration.getConnectionProfile().getBytes());
+            gateway = gateways.get(fabricId);
+            Gateway.Builder builder = null;
+
+            if (gateway == null) {
+                ByteArrayInputStream connProfileIS = new ByteArrayInputStream(ConnectionConfiguration.getConnectionProfile().getBytes());
                 WalletManagerDelegate wmd = new WalletManagerDelegate();
                 Wallet wallet = wmd.getWallet();
+                
                 try {
-                    builder = Gateway.createBuilder().identity(wallet, id).networkConfig(connProfileIS).discovery(true);
+                    builder = Gateway.createBuilder().identity(wallet, fabricId).networkConfig(connProfileIS).discovery(true);
                 } catch (IOException e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                    return null;
+                    throw new GatewayException("Error constructing gateway.", e);
                 }
-                //construct the gateway
+            
                 gateway = builder.connect();
-                gateways.put(id, gateway);
+                gateways.put(fabricId, gateway);
             } 
         }
         return gateway;
     }
 
-    public static Contract getContract(String identityId) {
-        
+    public static Contract getContract(String fabricId) throws IdentityException, GatewayException {
         String channel = ConnectionConfiguration.getChannel();
+        String chaincodeId = ConnectionConfiguration.getContractId();
 
-        return getGateway(identityId).getNetwork(channel).getContract(ConnectionConfiguration.getContractId());
+        Gateway gateway = getGateway(fabricId);
+        Network network = null;
+        Contract contract = null;
+        try {
+            network = gateway.getNetwork(channel);
+            contract = network.getContract(chaincodeId);
+        } catch (GatewayRuntimeException e) {
+            throw new GatewayException("Error retrieving contract.", e);
+        }
+        return contract;
     }
 }
