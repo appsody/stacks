77b0a322a66e0ef71301824727cfef4878628841
Terrence Nixa <tnixa@us.ibm.com>
2020-04-15 10:55:51 -0500

    wallet manager exception handling

--- incubator/blockchain-client-openliberty/templates/default/src/main/java/application/wm/WalletManager.java
+++ incubator/blockchain-client-openliberty/templates/default/src/main/java/application/wm/WalletManager.java
@@ -1,9 +1,10 @@
 package application.wm;
 
 import org.hyperledger.fabric.gateway.Wallet;
+import org.hyperledger.fabric_ca.sdk.exception.IdentityException;
 
 public interface WalletManager {
     
-    public Wallet getWallet();
+    public Wallet getWallet() throws IdentityException;
 
 }
