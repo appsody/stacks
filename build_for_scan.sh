fc3dfee67025e1561b5631fd2281261977963203
davco01a <dacohen@us.ibm.com>
2021-04-28 13:46:06 -0400

    no wget

--- build_for_scan.sh
+++ build_for_scan.sh
@@ -1,4 +1,3 @@
-wget -nv https://repo1.maven.org/maven2/org/osgi/org.osgi.core/6.0.0/org.osgi.core-6.0.0.jar
 javac -cp .:org.osgi.core-6.0.0.jar incubator/java-openliberty/image/project/util/RangeIncludesVersion.java
 rm -rf experimental
 cd incubator
