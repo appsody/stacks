ee3c5af29eda444b011b5f3130529f45b271b5d2
davco01a <dacohen@us.ibm.com>
2021-04-28 11:32:57 -0400

    compile only java file

---
+++ compile.sh
@@ -0,0 +1,2 @@
+wget -nv https://repo1.maven.org/maven2/org/osgi/org.osgi.core/6.0.0/org.osgi.core-6.0.0.jar
+javac -cp .:org.osgi.core-6.0.0.jar incubator/java-spring-boot2/image/project/util/RangeIncludesVersion.java
